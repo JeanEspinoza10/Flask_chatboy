16d0893d9d81dad86f0217a713cab4a7e2f8194c
JeanEspinoza10 <jeanria10@gmail.com>
2023-03-15 18:11:07 -0500

    ahora

--- config.py
+++ config.py
@@ -2,15 +2,7 @@ import os
 from datetime import timedelta
 
 class BaseConfig:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET')
-
-    MAIL_SERVER = os.getenv('MAIL_SERVER')
-    MAIL_PORT = os.getenv('MAIL_PORT')
-    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS')
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    pass
 
 
 class DevelopmentConfig(BaseConfig):
@@ -22,7 +14,6 @@ class DevelopmentConfig(BaseConfig):
 class ProductionConfig(BaseConfig):
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(hours=1)
-    MAIL_DEBUG = False
     PROPAGATE_EXCEPTIONS = True
 
 
