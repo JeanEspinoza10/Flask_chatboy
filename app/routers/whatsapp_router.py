10ae6a6b39193db3b114a502deeb1a628fee9834
JeanEspinoza10 <jeanria10@gmail.com>
2023-03-15 20:15:51 -0500

    subiendo

--- app/routers/whatsapp_router.py
+++ app/routers/whatsapp_router.py
@@ -2,6 +2,7 @@ from app import api
 from flask_restx import Resource
 from flask import request
 from app.controller.whatasapp_controller import Webhook
+import os
 webhook_ns = api.namespace(
     name="Webhook",
     description="Ruta para recibir y enviar",
@@ -12,8 +13,17 @@ webhook_ns = api.namespace(
 class Whatsapp(Resource):
     def get(self):
         query = request
-        controller = Webhook
-        return controller.VerifyToken(query)
+        try:
+            accessToken = os.getenv("ACCES_TOKEN")
+            token = query.args.get("hub.verify_token")
+            challenge = query.args.get("hub.challenge")
+            suscribe=query.args.get("hub.mode")
+            if token == accessToken:
+                return challenge
+            else:
+                return "No se valido correctamente", 400
+        except: 
+            return"Error", 400
     def post(self):
         query = request
         controller = Webhook
